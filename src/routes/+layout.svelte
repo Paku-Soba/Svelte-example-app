61e652b4e27fc94fad71a2162d33901b603db744
Paku-Soba <77097783+Paku-Soba@users.noreply.github.com>
2023-12-11 16:43:56 +0900

    [add] TailwindCSS反映検証

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '../app.css';
+</script>
+
+<div class="h-screen bg-background">
+    <main>
+        <div class="drawer">
+            <slot />
+        </div>
+    </main>
+</div>
