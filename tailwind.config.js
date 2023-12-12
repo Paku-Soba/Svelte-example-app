0b2353cd8a37597b78efa27343cc8f353aa028aa
Paku-Soba <77097783+Paku-Soba@users.noreply.github.com>
2023-12-12 17:01:58 +0900

    [fix] Installing Tailwind CSS as a PostCSS plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  // tailwindcss　ビルドプロセス失敗、反映されない状況
-  content: ["./src/**/*.{html,js,svelte}"],
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
